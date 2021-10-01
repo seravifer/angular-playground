a93d5789c8c27680a7e46ce8e9bb8835964aa8a7
seravifer <sergiavilafernandez@gmail.com>
2021-10-01 20:27:48 +0200

    feat: add storage service

---
+++ src/app/core/services/storage.service.ts
@@ -0,0 +1,34 @@
+import { Subject } from 'rxjs';
+import { Injectable } from "@angular/core"
+
+@Injectable({
+  providedIn: "root"
+})
+export class StorageService {
+
+  private changeSubject = new Subject<string | null>();
+  public change$ = this.changeSubject.asObservable();
+
+  constructor() { }
+
+  set<T = any>(key: string, data: T) {
+    const json = JSON.stringify(data);
+    localStorage.setItem(key, json);
+    this.changeSubject.next(key);
+  }
+
+  get<T = any>(key: string): T | null {
+    return JSON.parse(localStorage.getItem(key));
+  }
+
+  remove(key: string) {
+    localStorage.removeItem(key);
+    this.changeSubject.next(key);
+  }
+
+  clearAll() {
+    this.changeSubject.next();
+    localStorage.clear();
+  }
+
+}
