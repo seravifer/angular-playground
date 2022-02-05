33d770d1cacd3faa9499bb74a043befcad948093
seravifer <sergiavilafernandez@gmail.com>
2022-02-05 16:41:14 +0100

    feat: add form array

--- src/app/features/reactive-form/reactive-form.component.ts
+++ src/app/features/reactive-form/reactive-form.component.ts
@@ -1,31 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
-  styleUrls: ['./reactive-form.component.scss']
+  styleUrls: ['./reactive-form.component.scss'],
 })
 export class ReactiveFormComponent implements OnInit {
-
-  public newUserForm = new FormGroup({
+  public form = new FormGroup({
     id: new FormControl(null, [
       Validators.required,
-      Validators.pattern(/^[0-9]+$/)
+      Validators.pattern(/^[0-9]+$/),
     ]),
     name: new FormControl(null, [
       Validators.required,
-      Validators.pattern(/^[A-Z]*[a-z]+(\s[A-Z]*[a-z]+)*$/)
-    ])
+      Validators.pattern(/^[A-Z]*[a-z]+(\s[A-Z]*[a-z]+)*$/),
+    ]),
+    selectedCountries: new FormArray([]),
   });
 
-  constructor() { }
+  countries: Array<any> = [
+    { name: 'India', value: 'india' },
+    { name: 'France', value: 'france' },
+    { name: 'USA', value: 'USA' },
+    { name: 'Germany', value: 'germany' },
+    { name: 'Japan', value: 'Japan' },
+  ];
 
-  ngOnInit(): void {
+  constructor() {}
+
+  onCheckboxChange(checked: boolean) {
+    const selectedCountries = this.form.controls.selectedCountries as FormArray;
+    if (checked) {
+      selectedCountries.push(new FormControl(checked));
+    } else {
+      const index = selectedCountries.controls.findIndex(
+        (x) => x.value === checked
+      );
+      selectedCountries.removeAt(index);
+    }
   }
+
+  ngOnInit(): void {}
 
   onSubmit() {
+    console.log(this.form.value);
     // TODO
   }
-
 }
--- src/app/features/reactive-form/reactive-form.module.ts
+++ src/app/features/reactive-form/reactive-form.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormRoutingModule } from './reactive-form-routing.module';
 import { ReactiveFormComponent } from './reactive-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ReactiveFormComponent],
   imports: [
     CommonModule,
     ReactiveFormRoutingModule,
+    FormsModule,
     ReactiveFormsModule
   ]
 })
